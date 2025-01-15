bcc052cf8ec37b1b281caa0aa32be6440dcfb018
Joe-muraguri <joe.crrm@gmail.com>
2025-01-15 16:53:28 +0300

    Updated the static and templates

--- static/script.js
+++ static/script.js
@@ -1,16 +1,26 @@
 const selectPackage = (packageName) => {
-    document.getElementById("payment-section").style.display = "block";
+    document.getElementById("payment-modal").style.display = "flex";
     document.getElementById("selectedPackage").innerText = `You selected: ${packageName}`;
+    
+}
+
+const closeModal = (modalId) => {
+    document.getElementById(modalId).style.display = "none";
 }
 
 const initiatePayment = () => {
-    const phoneNumber = document.getElementById("phone-number").ariaValueMax;
+    const phoneNumber = document.getElementById("phone-number").value;
     if (!phoneNumber){
-        alert("Please enteryour phone number to initiate Mpesa payment");
+        alert("Please enter your phone number to initiate Mpesa payment");
         return;
     }
-    document.getElementById("payment-section").innerHTML = "<p>Processing Payment ...Please wait as we verity.</p>";
+    closeModal('payment-modal');
+    const processingModel = document.getElementById("processing-modal");
+    processingModel.style.display = "flex";
     setTimeout(()=>{
-        document.getElementById("success-animation").style.display = "block";
-    },5000)
+        processingModel.style.display = "none";
+        const successModal = document.getElementById("success-modal");
+        successModal.style.display = "flex";
+        setTimeout(()=> closeModal('success-modal'), 5000);
+    }, 5000);
 }
